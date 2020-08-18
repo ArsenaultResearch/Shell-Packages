ee48503565a6ac47ba24e312908e0fbe3d21dbcc
ArsenaultResearch <samarsenault93@gmail.com>
2020-08-18 17:49:08 -0400

    test

--- ATACseq_pipeline.sh
+++ ATACseq_pipeline.sh
@@ -6,7 +6,7 @@
 
 /project/bghlab/Sinv_Overwinter
 # only accessible through qlogin and xfer nodes. I recommend moving the .fq files to your scratch directory to work with
-# Files beginning with "SF" are "Spring Flying" and "OW" are "Overwintered". 
+# Files beginning with "SF" are "Spring Flying" and "OW" are "Overwintered".
 # "Br" indicates brain tissue, "Ov" indicates "Ovary"
 # Individual IDs are given by the number. OW_Br1.fq and OW_Ov1.fq are from the same individual (Though not the same as SF_Br1.fq and SF_Ov1.fq)
 
@@ -21,3 +21,5 @@ run_fastqc () {
 }
 
 run_fastqc OW_Br1.fastq
+
+## test
